24ab72083cd19421039f0566b3fc9f2c594719af
Beshoy Bebawe <bbebawe@outlook.com>
2020-11-02 20:26:58 +0000

    intial commit

---
+++ src/main/java/com/reply/streamingservicesapi/StreamingServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.reply.streamingservicesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingServicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reply/streamingservicesapi/StreamingServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.streamingservicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
